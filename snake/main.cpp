ec3ac44d34bb990a646df28ab3db61fd30112a60
Shion Fukuzawa <sfukuzawa14@Shion-Fukuzawa.local>
2015-11-07 20:41:30 +0900

    Initial Commit

---
+++ snake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  snake
+//
+//  Created by Shion Fukuzawa on 11/7/15.
+//  Copyright (c) 2015 Shion Fukuzawa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
